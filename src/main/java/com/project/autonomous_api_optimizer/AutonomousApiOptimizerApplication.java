bc2e9c0e82438bde2cba6366643610f43302999b
aayush <aayushchaudhari1412@gmail.com>
2025-05-15 11:32:15 +0530

    Initial Spring Boot project setup with pom.xml

---
+++ src/main/java/com/project/autonomous_api_optimizer/AutonomousApiOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.project.autonomous_api_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonomousApiOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonomousApiOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/autonomous_api_optimizer/AutonomousApiOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autonomous_api_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutonomousApiOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
